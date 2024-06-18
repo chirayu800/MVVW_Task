30d8a91025eb974bd30978620c153e2dd67e769f
chirayu Baij <chirayu200@gmail.com>
2024-06-18 13:48:12 +0545

    SignUp

---
+++ app/src/main/java/com/example/mvvw_task/view/SignUpActivity.kt
@@ -0,0 +1,29 @@
+package com.example.crud_assignments
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import com.example.crud_assignments.view.LoginActivity
+
+i
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        btnSignUp.setOnClickListener {
+            // Handle sign up button click
+            navigateToLogin()
+        }
+    }
+
+    private fun navigateToLogin() {
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+        startActivity(intent)
+        finish()
+    }
+}
